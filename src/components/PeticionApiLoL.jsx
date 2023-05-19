bb38cda51941a38c7fc1de6b98c904fa3f41623a
Didier <didier002010@gmail.com>
2023-05-19 14:07:44 -0500

    botones siguiente, atras y titulo

--- src/components/PeticionApiLoL.jsx
+++ src/components/PeticionApiLoL.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const PeticionApiLoL = () => {
+    const [campeones, setCampeones] = React.useState([]);
+    const [paginacion, setPaginacion] = React.useState(1);
+
+    
+
+
+
+
+
   return (
-    <div>PeticionApiLoL</div>
+    <div>
+<h1> Petición al api de League of legends </h1>
+
+<button>  Atrás  </button>
+<button>  Siguiente   </button>
+</div>
+   
   )
 }
+
 
 export default PeticionApiLoL
